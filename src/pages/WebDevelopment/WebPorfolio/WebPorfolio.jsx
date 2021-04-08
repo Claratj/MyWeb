c7ba82c85c8ca6cb8f3c0df09cd1588e1d92fb33
Clara <clara@claratoloba.com>
2021-04-08 13:51:47 +0200

    switch projects working

--- src/pages/WebDevelopment/WebPorfolio/WebPorfolio.jsx
+++ src/pages/WebDevelopment/WebPorfolio/WebPorfolio.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { ProjectList } from '../../../shared/ProjectList/ProjectList';
+
+import Projects from '../../../data/Projects.json';
 
 export default function WebPorfolio() {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        setProjects(Projects);
+    }, []);
+    let showLess = false;
+
     return (
         <div>
-
+            <ProjectList projects={projects} showLess={showLess} />
         </div>
     )
 }

9c3c7028dae2a49493e6c962d323bdcb36aed472
Clara <clara@claratoloba.com>
2021-04-13 15:53:31 +0200

    transparency handle on scroll header

--- src/core/Header/Header.jsx
+++ src/core/Header/Header.jsx
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link as LinkScroll } from 'react-scroll';
 
 
 import './Header.scss';
 import Logo1 from '../../assets/img/logo1-01.svg';
 export function Header() {
-
+    const [scrollNav, setScrollNav] = useState(false);
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
 
+    const changeNav = () => {
+
+        if (window.scrollY >= 80) {
+            setScrollNav(true);
+
+        } else {
+            setScrollNav(false);
+
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav)
+    }, [])
+
+
+
 
     return (
-        <nav className="header">
+        <nav className={scrollNav ?  'header active' : 'header'}>
             <figure className="figure">
                 <LinkScroll to="intro" smooth={true} duration={1000}><img src={Logo1} /></LinkScroll>
             </figure>

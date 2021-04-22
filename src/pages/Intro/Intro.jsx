a0249dd0d90b626d25d70cb0125d68df76cecddf
Clara <clara@claratoloba.com>
2021-04-22 15:37:24 +0200

    intro vibes

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 
+import { motion } from 'framer-motion';
+
 import './Intro.scss';
 
 export default function Intro() {
+
+    const variants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+    }
 
     return (
         <div className="main intro" id="intro">
             <div className="intro-txt" >
                 <h2 className="">Hola caracola</h2>
             </div>
-            <div className="circle"></div>
-            <div className="triangle"></div>
+            <motion.div drag="x"
+                dragConstraints={{ left: -200, right: 100 }} whileHover={{ scale: 7 }} whileTap={{ scale: 1 }} className="circle" />
+
+            <motion.div initial="hidden"
+                animate="visible"
+                variants={variants} className="triangle" whileHover={{ scale: 3 }} />
+
         </div>
     )
 }

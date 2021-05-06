644e7510bd2d0ca5bb7c4c50351ca713305e43ac
Clara <clara@claratoloba.com>
2021-05-06 15:34:09 +0200

    footer improve with other fonticons

--- src/core/Footer/Footer.jsx
+++ src/core/Footer/Footer.jsx
@@ -5,13 +5,13 @@ import './Footer.scss';
 export function Footer() {
     return (
         <footer>
-                <nav className="rrss">
-                    <a href="https://twitter.com/cofclarividence"><span className="icon-twitter" /></a>
-                    <a href="https://gitlab.com/Claratj"><span className="icon-gitlab" /></a>
-                    <a href="https://www.linkedin.com/in/claratoloba"><span className="icon-in" /></a>
-                    <a href="mailto:clara@claratoloba.com"><span className="icon-mail" /></a>
-                </nav>
-                <p className="">Made by Clara <span className="icon-copy" /> 2021</p>
+            <nav className="rrss">
+                <a href="https://twitter.com/cofclarividence"><span className="icon-twitter" /></a>
+                <a href="https://gitlab.com/Claratj"><span className="icon-git" /></a>
+                <a href="https://www.linkedin.com/in/claratoloba"><span className="icon-in" /></a>
+                <a href="mailto:clara@claratoloba.com"><span className="icon-paper-plane" /></a>
+            </nav>
+            <p className="">Made by Clara <span className="icon-copy" /> 2021</p>
         </footer>
     )
 }

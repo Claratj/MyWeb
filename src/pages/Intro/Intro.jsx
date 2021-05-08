a4db683fe5c2ccd338c60f25a028d1e8b79cc76a
Clara <clara@claratoloba.com>
2021-05-08 15:56:38 +0200

    wip

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -17,7 +17,7 @@ export default function Intro() {
             <div className="intro-txt" >
                 <motion.h2 className="">Hola caracola</motion.h2>
             </div>
-            <motion.div drag="x"
+            {/* <motion.div drag="x"
                 dragConstraints={{ left: -200, right: 100 }}
                 whileHover={{ scale: 12 }}
                 whileTap={{ scale: 1 }}
@@ -25,24 +25,9 @@ export default function Intro() {
                     type: 'spring',
                     stiffness: 300
                 }}
-                className="circle" />
+                className="circle" /> */}
 
-            <motion.div className="triangle" whileHover={{
-                scale: [1, 2, 10, 2, 1],
-                rotate: [0, 0, 270, 270, 0]
-            }} transition={{
-                duration: 1.2,
-                ease: "easeInOut",
-            }}
-                whileTap={{ scale: 1 }}
-            />
-            <motion.div whileHover={{ scale: 8, rotate: [0, 0, 20, 270, 0] }} transition={{
-                duration: 1,
-                ease: "easeInOut",
 
-            }}
-                whileTap={{ scale: 1 }}
-                className="hex" />
             <div className="waves">
 
             </div>

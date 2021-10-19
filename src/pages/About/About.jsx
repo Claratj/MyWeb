ccf78ca8285f6aa2ed7be6430a5e1ccd99b6f072
Clara <clara@claratoloba.com>
2021-10-19 18:01:06 +0200

    change about me description

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -59,7 +59,7 @@ export function About() {
                         <span className="">I'm Clara</span> </h2>
                 </figure>
                 <div className="about-description">
-                    <p>Based in Madrid, I'm a Web Developer focused on both Front-end and Back-end technologies.</p>
+                    <p>Based in Madrid, I'm a Web Developer focused on Front-end technologies.</p>
                     <p>I don't know if I found coding or coding found me. I started my career in Graphic Design but I soon realised that the new platforms were not going to be in ink and paper.
                         I found in Web Development a field of infinite possibilities where the technical base is complemented with creativity and good practices.</p>
                     <p>My previous experience has been very useful tool when approaching any project, since I take into account the technical approach to development together with the user experience point of view.</p>

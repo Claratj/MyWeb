e2e8e2aab7e09c1211347aa4e4de24fdf386fc0d
ClaraTJ <clara@claratoloba.com>
2021-12-20 15:55:16 +0100

    feat: Basic 404 page added.

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -5,12 +5,15 @@ import "./Error.scss";
 
 export default function Error() {
   return (
-    <div className="error_container">
+    <div className="error">
       <h1 className="section-title">Ups...</h1>
       <p className="error--txt">
-        It seems like the page you're looking for doesn't exits 🧐.
+        It seems like the page you're looking for doesn't exits{" "}
+        <span className="error--icon">🧐</span>.
       </p>
-      <Link to={"/"}>Let's leave the unknown for now 🏡</Link>
+      <Link to={"/"}>
+        Let's leave the unknown for now <span className="error--icon">🏡</span>
+      </Link>
     </div>
   );
 }

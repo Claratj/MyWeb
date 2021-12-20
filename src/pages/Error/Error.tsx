dba1328f8a02b60a36cc31db8c6b9546a0270bca
ClaraTJ <clara@claratoloba.com>
2021-12-20 15:49:23 +0100

    feat: Wip 404 page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Error.scss";
 
 export default function Error() {
   return (
-    <div>Ups, It seems like the page you're looking for doesn't exist.</div>
+    <div className="error_container">
+      <h1 className="section-title">Ups...</h1>
+      <p className="error--txt">
+        It seems like the page you're looking for doesn't exits 🧐.
+      </p>
+      <Link to={"/"}>Let's leave the unknown for now 🏡</Link>
+    </div>
   );
 }

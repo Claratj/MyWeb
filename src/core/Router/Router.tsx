d62d17042deb719a8dc32d451a3a8ec8475f4598
ClaraTJ <clara@claratoloba.com>
2021-12-20 10:53:05 +0100

    feat: Add an error page structure

--- src/core/Router/Router.tsx
+++ src/core/Router/Router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import GraphicPorfolio from "../../pages/Creative/GraphicPorfolio/GraphicPorfolio";
 import PhotographyPorfolio from "../../pages/Creative/PhotographyPorfolio/PhotographyPorfolio";
+import Error from "../../pages/Error/Error";
 
 import { Home } from "../../pages/Home";
 import WebPorfolio from "../../pages/WebDevelopment/WebPorfolio/WebPorfolio";
@@ -13,6 +14,7 @@ export function Router() {
       <Route path="/photography" element={<PhotographyPorfolio />} />
       <Route path="/graphic" element={<GraphicPorfolio />} />
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <div>Ups, It seems like the page you're looking for doesn't exist.</div>
+  );
+}
